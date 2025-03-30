ffacdd9979675a201ab935845ce2773eeb32cffe
Monobladegg <fushersf2@gmail.com>
2025-03-30 10:40:59 +0300

    Added swagger

--- src/gateway/exports.ts
+++ src/gateway/exports.ts
@@ -3,6 +3,7 @@ import { useExpressServer, useContainer } from "routing-controllers";
 
 import { diContainer } from "./di";
 import { logger } from "./logger";
+
 export * as dtos from "./dtos";
 
 useContainer(diContainer);
@@ -17,8 +18,15 @@ import {
   TrackBeforeMiddleware,
   TrackAfterMiddleware,
 } from "./middlewares";
+import { setupSwagger } from "./swagger";
 
 const app = express();
+
+const args = process.argv.slice(2);
+const enableSwagger = args.includes("swagger");
+
+
+if (enableSwagger) setupSwagger(app);
 
 const start = () => {
   useExpressServer(app, {
@@ -32,7 +40,6 @@ const start = () => {
       ErrorHandlerMiddleware,
     ],
   });
-
   return app.listen(3000, () => {
     logger.info("BackEnd started on 3000 port");
   });
---
+++ src/gateway/swagger/swagger.ts
@@ -0,0 +1,32 @@
+import swaggerUi, { JsonObject } from "swagger-ui-express";
+import { Express, Request, Response, NextFunction } from "express";
+import path from "path";
+import fs from "fs";
+import yaml from "js-yaml";
+import basicAuth from "basic-auth";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const yamlFilePath = path.join(__dirname, "./BookingYml.openapi.yaml");
+
+const swaggerDocument = yaml.load(fs.readFileSync(yamlFilePath, "utf8")) as JsonObject;
+
+const auth = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const user = basicAuth(req);
+  if (user && user.name === "swagger" && user.pass === process.env['SWAGGER_PASSWORD']) {
+    return next();
+  }
+
+  res.setHeader("WWW-Authenticate", 'Basic realm="Swagger UI"');
+  res.status(401).send("Access denied");
+  return Promise.resolve();
+};
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", auth, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+};
