37942f870c68573a62757b1ef2324c633aaf9761
Anton Muliarchyk <antowamulyarchik@gmail.com>
2025-07-16 14:56:36 +0300

    refactor: separate client and host booking workflows, remove generic booking sagas

--- src/booking/types/index.ts
+++ src/booking/types/index.ts
@@ -3,3 +3,5 @@ export type { BookingProperties } from "./BookingProperties";
 export type { UserProperties } from "./UserProperties";
 export type { ClientProperties } from "./ClientProperties";
 export type { UpdateBookingData } from "./UpdateBookingData";
+export type { UpdateHostData } from "./UpdateHostData";
+export type { UpdateClientData } from "./UpdateClientData";
